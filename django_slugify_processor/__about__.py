e049d2a0ee42a545c7ebf24f915d3c769544b2bc
Tony Narlock <tony@git-pull.com>
2017-11-26 14:46:20 -0600

    initial python package and __about__ metadata

---
+++ django_slugify_processor/__about__.py
@@ -0,0 +1,8 @@
+__title__ = 'django-slugify-processor'
+__package_name__ = 'django_slugify_processor'
+__version__ = '0.8'
+__description__ = 'pipeline for slugification edgecases in django'
+__email__ = 'i@devel.tech'
+__author__ = 'devel.tech'
+__license__ = 'BSD'
+__copyright__ = 'Copyright 2017 devel.tech'
