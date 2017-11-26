fa6da6bc24b3b4b56fafd1b0c54edb1ec5b9e684
Tony Narlock <tony@git-pull.com>
2017-11-26 14:45:29 -0600

    initial setup.py

---
+++ setup.py
@@ -0,0 +1,76 @@
+import sys
+
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+about = {}
+with open("django_slugify_processor/__about__.py") as fp:
+    exec(fp.read(), about)
+
+with open('requirements/base.txt') as f:
+    install_reqs = [line for line in f.read().split('\n') if line]
+
+with open('requirements/test.txt') as f:
+    tests_reqs = [line for line in f.read().split('\n') if line]
+
+if sys.version_info[0] > 2:
+    readme = open('README.rst', encoding='utf-8').read()
+else:
+    readme = open('README.rst').read()
+
+history = open('CHANGES').read().replace('.. :changelog:', '')
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+
+setup
+    name=about['__title__'],
+    version=about['__version__'],
+    url='http://github.com/develtech/django-slugify-processor/',
+    download_url='https://pypi.python.org/pypi/django-slugify-processor',
+    license=about['__license__'],
+    author=about['__author__'],
+    author_email=about['__email__'],
+    description=about['__description__'],
+    long_description=readme,
+    packages=['django_slugify_processor'],
+    include_package_data=True,
+    install_requires=install_reqs,
+    tests_require=tests_reqs,
+    cmdclass={'test': PyTest},
+    zip_safe=False,
+    keywords=about['__title__'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
+        'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Utilities',
+    ],
+)
