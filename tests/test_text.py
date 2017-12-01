359c5b9c92c9118c8207400d0563ed8958151e86
Tony Narlock <tony@git-pull.com>
2017-12-01 06:20:08 -0600

    sample test for settings

--- tests/test_text.py
+++ tests/test_text.py
@@ -1,6 +1,20 @@
+# -*- coding: utf-8 -*-
+
 from django_slugify_processor.text import slugify
 from django.utils.text import slugify as django_slugify
 
 
 def test_slugify_fallback_to_default():
     assert slugify('c++') == django_slugify('c++')
+
+
+def slugify_programming(value):
+    value = value.replace('c++', 'cpp')
+    return value
+
+
+def test_slugify_slugify_processors(settings):
+    settings.SLUGIFY_PROCESSORS = [
+        'tests.test_text.slugify_programming'
+    ]
+    assert slugify('c++') == 'cpp'
