adcec7a7300ad40d08e2d45b3b5d554a62689e10
Tony Narlock <tony@git-pull.com>
2017-11-30 21:36:05 -0600

    docstring updates

--- django_slugify_processor/text.py
+++ django_slugify_processor/text.py
@@ -5,14 +5,17 @@ from django.utils.text import slugify as django_slugify
 
 
 def slugify(value, allow_unicode=False):
-    """Override default slugify in django to handle programming symbols.
+    """Override default slugify in django to handle custom scenarios.
+
+    Run value through functions declared in SLUGIFY_PROCESSORS. The value is
+    then passed-through to django's slugify.
 
     :param value: string to slugify
     :type value: string
     :param allow_unicode: whether or not to allow unicode (e.g. chinese)
     :type allow_unicode: bool
 
-    How to slugify filters:
+    Examples of slugify processors, assume *project/app/slugify_processors.py*:
 
         def slugify_programming_languages(value):
             value = value.lower()
@@ -21,10 +24,28 @@ def slugify(value, allow_unicode=False):
             value = value.replace('c#', 'c-sharp')
             return value
 
+        def slugify_geo_acronyms(value):
+            value = value.lower()
+
+            value = value.replace('New York City', 'nyc')
+            value = value.replace('United States', 'usa')
+            return value
+
+        def slugify_us_currency(value):
+            value = value.lower()
+
+            value = value.replace('$', 'usd')
+            value = value.replace('US$', 'usd')
+            value = value.replace('US Dollar', 'usd')
+            value = value.replace('U.S. Dollar', 'usd')
+            return value
+
     Settings:
 
         SLUGIFY_PROCESSORS = [
-            'develtech.path.to.slugify_programming_languages'
+            'project.app.slugify_programming_languages',
+            'project.app.slugify_geo_acronyms',
+            'project.app.slugify_us_currency',
         ]
     """
     if hasattr(settings, 'SLUGIFY_PROCESSORS'):
