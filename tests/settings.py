3871247b95591efcb6e8e0ba53250594c3f50fc2
Tony Narlock <tony@git-pull.com>
2017-12-01 23:46:00 -0600

    add example cases for django-extensions

--- tests/settings.py
+++ tests/settings.py
@@ -1,1 +1,10 @@
 SECRET_KEY = 'dummy'
+
+DATABASES = {
+    'default': {
+        'NAME': ':memory:',
+        'ENGINE': 'django.db.backends.sqlite3',
+    }
+}
+
+INSTALLED_APPS = ['test_app']
--- tests/test_test_project.py
+++ tests/test_test_project.py
@@ -1,7 +1,28 @@
 # -*- coding: utf-8 -*-
+import pytest
+from django.apps import apps
 
 
+@pytest.mark.django_db
+def test_models_passthrough(settings):
+    MyModel = apps.get_model('test_app.MyModel')
+    entered = 'c++'
+    expected = 'c'
 
+    m = MyModel(title=entered)
+    m.save()
+
+    assert m.django_extensions_slug == expected
+
+
+@pytest.mark.django_db
 def test_models(settings):
-    settings.INSTALLED_APPS = ['test_app']
+    settings.SLUGIFY_PROCESSORS = ['test_app.coding.slugify_programming']
+    MyModel = apps.get_model('test_app.MyModel')
+    entered = 'c++'
+    expected = 'cpp'
 
+    m = MyModel(title=entered)
+    m.save()
+
+    assert m.django_extensions_slug == expected
