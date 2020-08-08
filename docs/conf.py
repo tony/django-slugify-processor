b9d97e67972b3943c6b7923398dbb7f0ec0f1dbb
Tony Narlock <tony@git-pull.com>
2020-08-08 13:10:08 -0500

    docs: Add icons, manifest

--- docs/conf.py
+++ docs/conf.py
@@ -19,6 +19,7 @@ with open("../django_slugify_processor/__about__.py") as fp:
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
+    'sphinx.ext.napoleon',
     'alagitpull',
     'sphinx_issues',
 ]
@@ -43,6 +44,7 @@ pygments_style = 'sphinx'
 
 html_theme_path = [alagitpull.get_path()]
 html_static_path = ['_static']
+html_extra_path = ['manifest.json']
 html_theme = 'alagitpull'
 html_sidebars = {
     '**': [
@@ -55,12 +57,19 @@ html_sidebars = {
 }
 
 html_theme_options = {
+    'logo': "img/icons/logo.svg",
     'github_user': 'develtech',
     'github_repo': 'django-slugify-processor',
     'github_type': 'star',
     'github_banner': True,
     'projects': alagitpull.projects,
     'project_name': about['__title__'],
+    'project_title': about['__title__'],
+    'project_description': about['__description__'],
+    'project_url': about['__docs__'],
+    'show_meta_manifest_tag': True,
+    'show_meta_og_tags': True,
+    'show_meta_app_icon_tags': True,
 }
 
 alagitpull_internal_hosts = [
