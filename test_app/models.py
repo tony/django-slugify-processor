79784a6878e92b15d7e829ecfb3fca33206548cd
Tony Narlock <tony@git-pull.com>
2023-12-09 07:38:22 -0600

    test_app(models): pydocstyle manual fixes

--- test_app/models.py
+++ test_app/models.py
@@ -1,3 +1,4 @@
+"""Django models for django-slugify-processor test app."""
 import django_extensions.db.fields
 from django.db import models
 
@@ -5,6 +6,8 @@ from django_slugify_processor.text import slugify
 
 
 class MyModel(models.Model):
+    """Test model for django-slugify-processor."""
+
     title = models.CharField(max_length=255)
     django_extensions_slug = django_extensions.db.fields.AutoSlugField(
         populate_from="title", slugify_function=slugify
