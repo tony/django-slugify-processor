cd782ceaadc63e0484a319f2714f9fd81952982c
Tony Narlock <tony@git-pull.com>
2023-12-09 06:33:27 -0600

    tests(text): pydocstyle manual fixes

--- tests/test_text.py
+++ tests/test_text.py
@@ -1,3 +1,4 @@
+"""Tests for django-slugify-processor's text utilities."""
 import typing as t
 
 from django.utils.text import slugify as django_slugify
@@ -6,9 +7,11 @@ from django_slugify_processor.text import slugify
 
 
 def test_slugify_fallback_to_default() -> None:
+    """Assert slugify() falls back to django's behavior."""
     assert slugify("c++") == django_slugify("c++")
 
 
 def test_slugify_slugify_processors(settings: t.Any) -> None:
+    """Assert slugify() follows SLUGIFY_PROCESSORS."""
     settings.SLUGIFY_PROCESSORS = ["test_app.coding.slugify_programming"]
     assert slugify("c++") == "cpp"
