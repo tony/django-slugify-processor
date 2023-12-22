49868a669bd59517cd5be895071f45724b6dd319
Tony Narlock <tony@git-pull.com>
2023-12-22 09:04:30 -0600

    docs(text[slugify]): Improve docstring

--- src/django_slugify_processor/text.py
+++ src/django_slugify_processor/text.py
@@ -7,15 +7,22 @@ from django.utils.text import slugify as django_slugify
 def slugify(value: str, allow_unicode: bool = False) -> str:
     """Override default slugify in django to handle custom scenarios.
 
-    Run value through functions declared in SLUGIFY_PROCESSORS. The value is
-    then passed-through to django's slugify.
+    Run value through functions declared in ``SLUGIFY_PROCESSORS`` setting. The value is
+    then passed-through to Django's :func:`django.utils.text.slugify()`.
 
     Parameters
     ----------
     value : str
-        string to slugify
+        A value such as an article name or page title, to "slugify", (turn into a
+        clean, URL-friendly short name)
     allow_unicode : bool
-        whether or not to allow unicode (e.g. chinese)
+        Whether or not to allow unicode (e.g. chinese).
+
+    Returns
+    -------
+    str
+        Clean, URL-friendly short name (a.k.a. "slug") of a string (e.g. a page or
+        article name).
 
     Examples
     --------
