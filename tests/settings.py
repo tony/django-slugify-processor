82dd362b83b252a72a5b24fec68d5659bfe347c7
Tony Narlock <tony@git-pull.com>
2024-12-18 17:50:40 -0600

    chore(ruff) Automated fixes for Python 3.9
    ruff check . --select ALL --fix --unsafe-fixes --preview --show-fixes --ignore T201 --ignore PT014 --ignore RUF100; ruff format .;
    
    Fixed 8 errors:
    - docs/conf.py:
        6 × UP006 (non-pep585-annotation)
    - tests/settings.py:
        2 × UP006 (non-pep585-annotation)
    
    Found 49 errors (8 fixed, 41 remaining).
    14 files left unchanged

--- tests/settings.py
+++ tests/settings.py
@@ -5,10 +5,10 @@ import typing as t
 
 SECRET_KEY: str = os.getenv("SECRET_KEY", "dummy")
 
-DATABASES: t.Dict[str, t.Any] = {
+DATABASES: dict[str, t.Any] = {
     "default": {"NAME": ":memory:", "ENGINE": "django.db.backends.sqlite3"},
 }
 
-INSTALLED_APPS: t.List[str] = ["test_app"]
+INSTALLED_APPS: list[str] = ["test_app"]
 
 USE_TZ: bool = False
